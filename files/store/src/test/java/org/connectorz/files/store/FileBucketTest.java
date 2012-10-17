451cc16a4bed9a8c0840b762280837d086f0290c
mvitz <vitz.michael@gmail.com>
2012-10-17 11:09:02 +0200

    Write can now be called multiple times without commiting in between.

--- files/store/src/test/java/org/connectorz/files/store/FileBucketTest.java
+++ files/store/src/test/java/org/connectorz/files/store/FileBucketTest.java
@@ -81,4 +81,21 @@ public class FileBucketTest {
         actual = this.cut.fetch(key);
         assertNull(actual);
     }
+
+    @Test
+    public void writeMultipleTimes() throws Exception {
+        final String key = "multiWrite";
+
+        this.cut.begin();
+        final byte[] firstEntry = "hello ".getBytes();
+        this.cut.write(key, firstEntry);
+
+        final byte[] secondEntry = "world!".getBytes();
+        this.cut.write(key, secondEntry);
+        this.cut.commit();
+
+        final byte[] actual = this.cut.fetch(key);
+        assertThat(actual, is("hello world!".getBytes()));
+    }
+
 }
