f66c263db9393d081dc1669798f49d9a27a6a492
Jonathan S. Fisher <jonathan@springventuregroup.com>
2015-10-15 18:08:06 -0500

    Fix https://github.com/AdamBien/connectorz/issues/13

--- files/store/src/main/java/org/connectorz/files/store/FileBucketStore.java
+++ files/store/src/main/java/org/connectorz/files/store/FileBucketStore.java
@@ -16,17 +16,20 @@ limitations under the License.
 package org.connectorz.files.store;
 
 import java.io.PrintWriter;
+
 import javax.naming.Reference;
 import javax.resource.ResourceException;
 import javax.resource.spi.ConnectionManager;
 import javax.resource.spi.ConnectionRequestInfo;
 import javax.resource.spi.ManagedConnectionFactory;
+
+import org.connectorz.files.Bucket;
 import org.connectorz.files.BucketStore;
 
 public class FileBucketStore
         implements BucketStore {
-
-    private ManagedConnectionFactory mcf;
+    private static final long serialVersionUID = 1L;
+	private ManagedConnectionFactory mcf;
     private Reference reference;
     private ConnectionManager cm;
     private PrintWriter out;
@@ -38,15 +41,16 @@ public class FileBucketStore
         this.out = out;
     }
 
-    @Override
-    public FileBucket getBucket(){
-        out.println("#FileBucketStore.getConnection " + this.cm + " MCF: " + this.mcf);
-        try {
-            return (FileBucket) cm.allocateConnection(mcf, getConnectionRequestInfo());
-        } catch (ResourceException ex) {
-            throw new RuntimeException(ex.getMessage());
-        }
-    }
+	@Override
+	public Bucket getBucket() {
+		out.println("#FileBucketStore.getConnection " + this.cm + " MCF: " + this.mcf);
+		try {
+			return (Bucket) cm.allocateConnection(mcf, getConnectionRequestInfo());
+		} catch (ResourceException ex) {
+			throw new RuntimeException(ex.getMessage());
+		}
+	}
+
    
     @Override
     public void setReference(Reference reference) {
